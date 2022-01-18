d67a2bfb480ed54370433a5c252dcc6ecb93425c
md2620 <78766492+md2620@users.noreply.github.com>
2022-01-18 16:46:17 +0000

    Delete Calibration_job_ssh.sh

--- Calibration_job_ssh.sh
+++
@@ -1,9 +0,0 @@
-#PBS -l walltime=12:00:00
-#PBS -l select=1:ncpus=4:mem=20gb
-
-module load anaconda3/personal
-source activate R-env
-
-cd /rds/general/user/md2620/home/asthma/Malo/R_Scripts
-
-Rscript Calibration_job.R
